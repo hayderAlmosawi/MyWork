10fc0dfe0da274189b97be52ca06eeb9b2615d69
hayderAlmosawi <hayder719@gmail.com>
2016-03-12 13:37:03 +0300

    Added files via upload

---
+++ firstsite/static_files/js/slide-menu.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+       isClosed = false;
+
+    function buttonSwitch() {
+
+        if (isClosed === true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    trigger.click(function () {
+        buttonSwitch();
+    });
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+});
+
+
+
+$('.carousel').carousel({
+    interval:4000
+})
